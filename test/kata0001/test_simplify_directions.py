72f2cb3c4783ac20c70a50b294c95d947e1c36cf
Donald Futers <donald.futers@gmail.com>
2024-12-06 21:58:16 +0000

    KATA 0001: write and pass second test on simplify_directions()

--- test/kata0001/test_simplify_directions.py
+++ test/kata0001/test_simplify_directions.py
@@ -10,17 +10,24 @@ def test_removes_single_north_south_cancellation():
     test_input = ["SOUTH", "NORTH", "EAST"]
     assert simplify_directions(test_input) == ["EAST"]
 
-@pytest.mark.skip
+
 def test_removes_single_east_west_cancellation():
-    pass
+    test_input = ["NORTH", "EAST", "WEST"]
+    assert simplify_directions(test_input) == ["NORTH"]
+
+    test_input = ["WEST", "EAST", "SOUTH"]
+    assert simplify_directions(test_input) == ["SOUTH"]
+
 
 @pytest.mark.skip
 def test_removes_multiple_north_south_cancellations():
     pass
 
+
 @pytest.mark.skip
 def test_removes_multiple_east_west_cancellations():
     pass
+
 
 @pytest.mark.skip
 def test_simplifies_while_retaining_multiple_moves_in_same_direction():
