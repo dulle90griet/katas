776ec14a40e3108900d9e719ac807efe5c07139e
Donald Futers <donald.futers@gmail.com>
2024-12-06 21:43:31 +0000

    KATA 0001: write first test for simplify_directions()

--- test/kata0001/test_simplify_directions.py
+++ test/kata0001/test_simplify_directions.py
@@ -4,7 +4,11 @@ from src.kata0001.simplify_directions import simplify_directions
 
 
 def test_removes_single_north_south_cancellation():
-    pass
+    test_input = ["NORTH", "SOUTH", "WEST"]
+    assert simplify_directions(test_input) == "WEST"
+
+    test_input = ["SOUTH", "NORTH", "EAST"]
+    assert simplify_directions(test_input) == "EAST"
 
 @pytest.mark.skip
 def test_removes_single_east_west_cancellation():
