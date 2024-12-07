69a29c09454cc40bea4f146d649bf027c5324f56
Donald Futers <donald.futers@gmail.com>
2024-12-07 14:25:25 +0000

    KATA 0002: pass fourth test on get_sub_square()

--- src/kata0002/get_sub_square.py
+++ src/kata0002/get_sub_square.py
@@ -1,4 +1,8 @@
 def get_sub_square(matrix, x_offset, y_offset):
+    if(x_offset > len(matrix[0]) - 3
+       or y_offset > len(matrix) - 3):
+        return None
+
     sub_square = []
     for y in range(y_offset, y_offset + 3):
         sub_row = []
