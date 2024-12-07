9fabceea824e166ddade4b924b3be46df4d1e9ae
Donald Futers <donald.futers@gmail.com>
2024-12-07 14:09:19 +0000

    KATA 0002: write first test for get_sub_square()

--- test/kata0002/test_get_sub_square.py
+++ test/kata0002/test_get_sub_square.py
@@ -3,17 +3,33 @@ import pytest
 from src.kata0002.get_sub_square import get_sub_square
 
 
-def test_returns_subsquare_with_zero_coords():
+def test_returns_sub_square_with_zero_coords():
+    matrix = [
+        [5, 3, 4, 6, 7, 8, 9, 1, 2],
+        [6, 7, 2, 1, 9, 5, 3, 4, 8],
+        [1, 9, 8, 3, 4, 2, 5, 6, 7],
+        [8, 5, 9, 7, 4, 1, 4, 2, 3],
+        [4, 2, 6, 8, 5, 3, 7, 9, 1],
+        [7, 1, 3, 9, 2, 4, 8, 5, 6],
+        [9, 6, 1, 5, 3, 7, 2, 8, 4],
+        [2, 8, 7, 4, 1, 9, 6, 3, 5],
+        [3, 4, 5, 2, 8, 6, 1, 7, 9]
+    ]
+    result = get_sub_square(matrix, 0, 0)
+    assert result == [
+        [5, 3, 4],
+        [6, 7, 2],
+        [1, 9, 8]
+    ]
+
+
+@pytest.mark.skip
+def test_returns_sub_square_with_identical_non_zero_coords():
     pass
 
 
 @pytest.mark.skip
-def test_returns_subsquare_with_identical_non_zero_coords():
-    pass
-
-
-@pytest.mark.skip
-def test_returns_subsquare_with_non_identical_non_zero_coords():
+def test_returns_sub_square_with_non_identical_non_zero_coords():
     pass
 
 
