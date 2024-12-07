b8d6e62721b2db3beae0576c438b593ab77a225d
Donald Futers <donald.futers@gmail.com>
2024-12-07 14:24:57 +0000

    KATA 0002: write fourth test for get_sub_square()

--- test/kata0002/test_get_sub_square.py
+++ test/kata0002/test_get_sub_square.py
@@ -75,6 +75,18 @@ def test_returns_sub_square_with_non_identical_non_zero_coords():
     ]
 
 
-@pytest.mark.skip
 def test_returns_None_for_coords_out_of_bounds():
-    pass
+    matrix = [
+        [5, 3, 4, 6, 7, 8, 9, 1, 2],
+        [6, 7, 2, 1, 9, 5, 3, 4, 8],
+        [1, 9, 8, 3, 4, 2, 5, 6, 7],
+        [8, 5, 9, 7, 4, 1, 4, 2, 3],
+        [4, 2, 6, 8, 5, 3, 7, 9, 1],
+        [7, 1, 3, 9, 2, 4, 8, 5, 6],
+        [9, 6, 1, 5, 3, 7, 2, 8, 4],
+        [2, 8, 7, 4, 1, 9, 6, 3, 5],
+        [3, 4, 5, 2, 8, 6, 1, 7, 9]
+    ]
+    assert get_sub_square(matrix, 7, 5) is None
+    assert get_sub_square(matrix, 10, 200) is None
+    assert get_sub_square(matrix, 3, 8) is None
