5ed8bf208512321104557dabe96fd2d0220e912b
Donald Futers <donald.futers@gmail.com>
2024-12-07 14:17:46 +0000

    KATA 0002: pass second test on get_sub_square()

--- src/kata0002/get_sub_square.py
+++ src/kata0002/get_sub_square.py
@@ -1,8 +1,8 @@
 def get_sub_square(matrix, x_offset, y_offset):
     sub_square = []
-    for y in range(0, 3):
+    for y in range(y_offset, y_offset + 3):
         sub_row = []
-        for x in range(0, 3):
+        for x in range(x_offset, x_offset + 3):
             sub_row.append(matrix[y][x])
         sub_square.append(sub_row)
     
