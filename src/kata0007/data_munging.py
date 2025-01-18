64253f11045273ca7c56b5f125719dc935844833
Donald Futers <donald.futers@gmail.com>
2025-01-18 23:37:36 +0000

    🎨 run black

--- src/kata0007/data_munging.py
+++ src/kata0007/data_munging.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import re
+from io import StringIO
 
 """ CodeKata.com Kata 04: Data Munging
 Available here: http://codekata.com/kata/kata04-data-munging/
@@ -9,24 +10,20 @@ Part One: Weather Data
 In weather.dat you'll find daily weather data for Morristown, NJ for June 2002. Download this text file, then write a program to output the day number (column one) with the smallest temperature spread (the maximum temperature is the second column, the minimum the third column).
 """
 
-def find_smallest_spread_ORIGINAL():
-    weather_df = pd.read_csv(
-        'src/kata0007/data/weather.dat',
-        sep=r"[\s*]+"
-    )
 
-    weather_df = weather_df[weather_df["Dy"] != "mo"] \
-                    .convert_dtypes()
-    
+def find_smallest_spread_ORIGINAL():
+    weather_df = pd.read_csv("src/kata0007/data/weather.dat", sep=r"[\s*]+")
+
+    weather_df = weather_df[weather_df["Dy"] != "mo"].convert_dtypes()
+
     weather_df["T_spread"] = weather_df["MxT"] - weather_df["MnT"]
 
     min_spread = weather_df["T_spread"].min()
-    day = weather_df.loc[
-        weather_df["T_spread"].idxmin(),
-        "Dy"
-    ]
+    day = weather_df.loc[weather_df["T_spread"].idxmin(), "Dy"]
 
-    print(f"Day {day} saw the smallest temperature spread, at {min_spread} degrees.")
+    print(
+        f"Day {day} saw the smallest temperature spread, at {min_spread} degrees."
+    )
 
 
 """ Part Two: Soccer League Table
@@ -35,29 +32,27 @@ The file football.dat contains the results from the English Premier League for 2
 
 
 def find_smallest_goal_difference_ORIGINAL():
-    football_df = pd.read_csv(
-        'src/kata0007/data/football.dat',
-        sep=r"[\s-]+"
-    )
+    football_df = pd.read_csv("src/kata0007/data/football.dat", sep=r"[\s-]+")
 
-    football_df = football_df.dropna(thresh=football_df.shape[1]) \
-                    .convert_dtypes()
-    
+    football_df = football_df.dropna(
+        thresh=football_df.shape[1]
+    ).convert_dtypes()
+
     football_df["goal_diff"] = abs(football_df["F"] - football_df["A"])
 
     min_diff = football_df["goal_diff"].min()
-    team = football_df.loc[
-        football_df["goal_diff"].idxmin(),
-        "Team"
-    ]
+    team = football_df.loc[football_df["goal_diff"].idxmin(), "Team"]
 
-    print(f"{team} had the smallest difference ({min_diff}) between 'for' and 'against' goals.")
+    print(
+        f"{team} had the smallest difference ({min_diff}) between 'for' and 'against' goals."
+    )
 
 
 """ Part Three: DRY Fusion
 
 Take the two programs written previously and factor out as much common code as possible, leaving you with two smaller programs and some kind of shared functionality.
 """
+
 
 def find_min_diff_and_label(df_to_analyse, column_a, column_b, label_column):
     df_to_analyse = df_to_analyse.convert_dtypes()
@@ -68,8 +63,7 @@ def find_min_diff_and_label(df_to_analyse, column_a, column_b, label_column):
 
     min_diff = df_to_analyse["comparison_column"].min()
     label = df_to_analyse.loc[
-        df_to_analyse["comparison_column"].idxmin(),
-        label_column
+        df_to_analyse["comparison_column"].idxmin(), label_column
     ]
 
     return min_diff, label
@@ -77,30 +71,29 @@ def find_min_diff_and_label(df_to_analyse, column_a, column_b, label_column):
 
 def find_smallest_spread():
     weather_df = pd.read_csv(
-        'src/kata0007/data/weather.dat',
-        sep=r"[\s*]+",
-        engine='python'
+        "src/kata0007/data/weather.dat", sep=r"[\s*]+", engine="python"
     )
     weather_df = weather_df[weather_df["Dy"] != "mo"]
 
     min_spread, day = find_min_diff_and_label(weather_df, "MxT", "MnT", "Dy")
 
-    print(f"Day {day} saw the smallest temperature spread, at {min_spread} degrees.")
+    print(
+        f"Day {day} saw the smallest temperature spread, at {min_spread} degrees."
+    )
 
 
 def find_smallest_goal_difference():
     football_df = pd.read_csv(
-        'src/kata0007/data/football.dat',
-        sep=r"[\s-]+",
-        engine='python'
+        "src/kata0007/data/football.dat", sep=r"[\s-]+", engine="python"
     ).dropna(thresh=8)
 
     min_diff, team = find_min_diff_and_label(football_df, "F", "A", "Team")
 
-    print(f"{team} had the smallest difference ({min_diff}) between 'for' and 'against' goals.")
+    print(
+        f"{team} had the smallest difference ({min_diff}) between 'for' and 'against' goals."
+    )
 
 
-
-if(__name__ == "__main__"):
+if __name__ == "__main__":
     find_smallest_spread()
     find_smallest_goal_difference()
--- test/kata0007/test_data_munging.py
+++ test/kata0007/test_data_munging.py
@@ -1,14 +1,20 @@
 import pandas as pd
 
-from src.kata0007.data_munging import find_smallest_spread, find_smallest_goal_difference, find_min_diff_and_label
+from src.kata0007.data_munging import (
+    find_smallest_spread,
+    find_smallest_goal_difference,
+    find_min_diff_and_label,
+)
 
 
 def test_find_min_diff_and_label_returns_expected_tuple_for_positive_diffs():
-    test_df = pd.DataFrame.from_dict([
-        {"label": 0, "a": 13, "b": 9},
-        {"label": 1, "a": 27, "b": 2},
-        {"label": 2, "a": 300, "b": 299}
-    ])
+    test_df = pd.DataFrame.from_dict(
+        [
+            {"label": 0, "a": 13, "b": 9},
+            {"label": 1, "a": 27, "b": 2},
+            {"label": 2, "a": 300, "b": 299},
+        ]
+    )
     output = find_min_diff_and_label(test_df, "a", "b", "label")
     assert type(output) is tuple
     min_diff, label = output
@@ -17,11 +23,13 @@ def test_find_min_diff_and_label_returns_expected_tuple_for_positive_diffs():
 
 
 def test_find_min_diff_and_label_returns_expected_tuple_for_mixed_diffs():
-    test_df = pd.DataFrame.from_dict([
-        {"label": 0, "a": 13, "b": 27},
-        {"label": 1, "a": 27, "b": 2},
-        {"label": 2, "a": 299, "b": 300}
-    ])
+    test_df = pd.DataFrame.from_dict(
+        [
+            {"label": 0, "a": 13, "b": 27},
+            {"label": 1, "a": 27, "b": 2},
+            {"label": 2, "a": 299, "b": 300},
+        ]
+    )
     min_diff, label = find_min_diff_and_label(test_df, "a", "b", "label")
     assert min_diff == 1
     assert label == 2
@@ -29,7 +37,7 @@ def test_find_min_diff_and_label_returns_expected_tuple_for_mixed_diffs():
 
 def test_find_smallest_spread_outputs_smallest_spread(capsys):
     find_smallest_spread()
-    
+
     stdout, _ = capsys.readouterr()
 
     assert "Day 14 saw the smallest temperature spread, at 2 degrees." in stdout
@@ -40,4 +48,7 @@ def test_find_smallest_goal_difference_outputs_smallest_difference(capsys):
 
     stdout, _ = capsys.readouterr()
 
-    assert "Aston_Villa had the smallest difference (1) between 'for' and 'against' goals." in stdout
+    assert (
+        "Aston_Villa had the smallest difference (1) between 'for' and 'against' goals."
+        in stdout
+    )
