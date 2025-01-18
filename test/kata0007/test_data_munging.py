fe0852a09ef0792a5bb9ab3dc4fe0786af31cfef
Donald Futers <donald.futers@gmail.com>
2025-01-18 20:59:34 +0000

    ✅ KATA0007: define test on Part Two

--- test/kata0007/test_data_munging.py
+++ test/kata0007/test_data_munging.py
@@ -1,4 +1,4 @@
-from src.kata0007.data_munging import find_smallest_spread
+from src.kata0007.data_munging import find_smallest_spread, find_smallest_goal_difference
 
 def test_find_smallest_spread_outputs_smallest_spread(capsys):
     find_smallest_spread()
@@ -6,3 +6,11 @@ def test_find_smallest_spread_outputs_smallest_spread(capsys):
     stdout, _ = capsys.readouterr()
 
     assert "Day 14 saw the smallest temperature spread, at 2 degrees." in stdout
+
+
+def test_find_smallest_goal_difference_outputs_smallest_difference(capsys):
+    find_smallest_goal_difference()
+
+    stdout, _ = capsys.readouterr()
+
+    assert "Aston_Villa had the smallest difference (1) between 'for' and 'against' goals." in stdout
