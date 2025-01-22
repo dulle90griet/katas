d694ce3cc01d05f0e3c42e1f2dfac8f1f4809c5f
Donald Futers <donald.futers@gmail.com>
2025-01-22 00:07:14 +0000

    🎉 KATA 0009: save instructions

---
+++ src/kata0009/merge_sorted_list.py
@@ -0,0 +1,6 @@
+"""
+Given two integer lists sorted in ascending order, `nums1` and `nums2`, and two integers, `m` and `n`, representing the number of elements to be used from the beginnings of `nums1` and `nums2` respectively, merge the two lists into a single list sorted in ascending order. `nums1` is assumed to have a length of m + n, and the final sorted list should be produced by modifying `nums1` in place, not by returning a new list.
+"""
+
+def merge(nums1, m, nums2, n):
+    pass
