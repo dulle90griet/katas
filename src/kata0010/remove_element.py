d8c1a4272d68ce82ea4e80abb4ad8e5bddd2af78
Donald Futers <donald.futers@gmail.com>
2025-01-24 15:52:04 +0000

    ⚡️ KATA 0010, part 1: refactor remove_element() for faster performance

--- src/kata0010/remove_element.py
+++ src/kata0010/remove_element.py
@@ -1,6 +1,15 @@
 def remove_element(nums: list[int], val: int) -> None:
-    for i in range(len(nums)-1, -1, -1):
-        if nums[i] == val:
-            nums.pop(i)
+    # first attempt - approaches O(n^2) complexity
+    # for i in range(len(nums)-1, -1, -1):
+    #     if nums[i] == val:
+    #         nums.pop(i)
     
-    return len(nums)
+    # return len(nums)
+
+    # second attempt - O(n) complexity
+    j = 0
+    for i in range(len(nums)):
+        if nums[i] != val:
+            nums[j] = nums[i]
+            j += 1
+    return j
--- test/kata0010/test_remove_element.py
+++ test/kata0010/test_remove_element.py
@@ -4,33 +4,33 @@ from src.kata0010.remove_element import remove_element
 def test_remove_element_modifies_list_in_place():
     nums_1 = [1, 2, 4, 2]
     nums_1_id = id(nums_1)
-    remove_element(nums_1, 2)
-    assert nums_1 != [1, 2, 4, 2]
+    k = remove_element(nums_1, 2)
+    assert nums_1[:k] != [1, 2, 4, 2]
     assert id(nums_1) == nums_1_id
 
     nums_2 = [29, 32, 48, 22, 9]
     nums_2_id = id(nums_2)
-    remove_element(nums_2, 32)
-    assert nums_2 != [29, 32, 48, 22, 9]
+    k = remove_element(nums_2, 32)
+    assert nums_2[:k] != [29, 32, 48, 22, 9]
     assert id(nums_2) == nums_2_id
 
 
 def test_remove_element_does_nothing_if_value_not_in_list():
     nums = [29, 32, 48, 22, 9]
     nums_id = id(nums)
-    remove_element(nums, 6)
-    assert nums == [29, 32, 48, 22, 9]
+    k = remove_element(nums, 6)
+    assert nums[:k] == [29, 32, 48, 22, 9]
     assert id(nums) == nums_id
 
 
 def test_remove_element_removes_all_instances_of_value_from_list():
     nums_1 = [29, 32, 48, 22, 9]
-    remove_element(nums_1, 32)
-    assert nums_1 == [29, 48, 22, 9]
+    k = remove_element(nums_1, 32)
+    assert nums_1[:k] == [29, 48, 22, 9]
 
     nums_2 = [1, 3, 2, 3, 4, 3, 8, 3, 16, 3, 32, 3, 64]
-    remove_element(nums_2, 3)
-    assert nums_2 == [1, 2, 4, 8, 16, 32, 64]
+    k = remove_element(nums_2, 3)
+    assert nums_2[:k] == [1, 2, 4, 8, 16, 32, 64]
 
 
 def test_remove_element_returns_number_of_elements_not_equal_to_value():
