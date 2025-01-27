e10e571e240aaee44bdd8365ea3bae9575bd72cc
Donald Futers <donald.futers@gmail.com>
2025-01-27 10:11:16 +0000

    ⚡️ KATA 0010, part 3: refactor two_sum() for faster (O(n)) performance

--- src/kata0010/pt3_two_sum.py
+++ src/kata0010/pt3_two_sum.py
@@ -1,7 +1,15 @@
 def two_sum(nums: list[int], target: int) -> list[int]:
-    # brute-force approach - O(n^2)
-    n = len(nums)
-    for i in range(n-1):
-        for j in range(i+1, n):
-            if nums[i] + nums[j] == target:
-                return [i, j]
+    # # brute-force approach - O(n^2)
+    # n = len(nums)
+    # for i in range(n-1):
+    #     for j in range(i+1, n):
+    #         if nums[i] + nums[j] == target:
+    #             return [i, j]
+
+    # hash table approach - O(n)
+    num_map = {}
+    for i in range(len(nums)):
+        complement = target - nums[i]
+        if complement in num_map:
+            return [num_map[complement], i]
+        num_map[nums[i]] = i
