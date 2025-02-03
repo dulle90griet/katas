fdf9b6b9dca760325747c3e0902752b3729c2feb
Donald Futers <donald.futers@gmail.com>
2025-02-03 10:44:15 +0000

    ✅ KATA 0011, pt. 1: pass all tests on is_palindrome() with string solution

--- src/kata0011/pt_1_is_palindrome.py
+++ src/kata0011/pt_1_is_palindrome.py
@@ -1,4 +1,5 @@
 """ Write a function that returns `True` if an integer is a palindrome, or `False` if it isn't. """
 
-def is_palindrome():
-    pass
+def is_palindrome(num: int) -> bool:
+    # string solution
+    return str(num) == str(num)[::-1]
