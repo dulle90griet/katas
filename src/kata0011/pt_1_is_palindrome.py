8111001f9cb5319d56e9ec697fa8111abbc45805
Donald Futers <donald.futers@gmail.com>
2025-02-03 10:51:16 +0000

    🔨 KATA 0011, pt. 1: implement alternative is_palindrome() solution without using str()

--- src/kata0011/pt_1_is_palindrome.py
+++ src/kata0011/pt_1_is_palindrome.py
@@ -1,5 +1,18 @@
 """ Write a function that returns `True` if an integer is a palindrome, or `False` if it isn't. """
 
 def is_palindrome(num: int) -> bool:
-    # string solution
-    return str(num) == str(num)[::-1]
+    # # string solution
+    # return str(num) == str(num)[::-1]
+
+    # stringless solution
+    if num < 0: return False
+
+    if num != 0 and num % 10 == 0:
+        return False
+
+    reversed_rear = 0
+    while num > reversed_rear:
+        reversed_rear = (reversed_rear * 10) + (num % 10)
+        num //= 10
+    
+    return num == reversed_rear or num == reversed_rear // 10
