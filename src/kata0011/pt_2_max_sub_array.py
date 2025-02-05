fe7e40b4260a48bb0736cffb2083a5e10321daa5
Donald Futers <donald.futers@gmail.com>
2025-02-05 23:22:26 +0000

    🔨 KATA 11, pt. 2: complete solution for max_sub_array()

---
+++ src/kata0011/pt_2_max_sub_array.py
@@ -0,0 +1,12 @@
+""" Given an integer array, `nums`, find the subarray with the largest sum, and return its sum. """
+
+def max_sub_array(nums: list[int]):
+    max = nums[0]
+    cur = 0
+    for i in range(1, len(nums)):
+        cur += nums[i]
+        if cur > max:
+            max = cur
+        elif cur < 0:
+            cur = 0
+    return max
