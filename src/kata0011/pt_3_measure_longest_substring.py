a4a3dc3fa4aec5981e05002413ebf5fddfc9a144
Donald Futers <donald.futers@gmail.com>
2025-02-07 16:54:45 +0000

    🔨 KATA 11, pt. 3: complete solution for longest_substring_len()

---
+++ src/kata0011/pt_3_measure_longest_substring.py
@@ -0,0 +1,15 @@
+def longest_substring_len(self, s: str) -> int:
+    maxlen = curlen = 1
+
+    j = 0
+    for i in range(1, len(s)):
+        if s[i] in i[j:i]:
+            j = i
+            curlen = 1
+            continue
+        
+        curlen += 1
+        if curlen > maxlen:
+            maxlen = curlen
+
+    return maxlen
