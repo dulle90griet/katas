b02f04036efd3ddea419124dbcbfa7fe75dd39a5
Donald Futers <donald.futers@gmail.com>
2025-02-14 16:09:53 +0000

    ✅ KATA 0012: define tests on find_longest_palindrome()

---
+++ src/kata0012/find_longest_palindromic_substring.py
@@ -0,0 +1,2 @@
+def find_longest_palindrome(s: str) -> str:
+    pass
---
+++ test/kata0012/test_find_longest_palindromic_substring.py
@@ -0,0 +1,44 @@
+from src.kata0012.find_longest_palindromic_substring import find_longest_palindrome
+
+
+def test_returns_single_char_if_no_palindromes():
+    assert find_longest_palindrome("a") == "a"
+
+    result = find_longest_palindrome("abc")
+    assert len(result) == 1
+    assert result in "abc"
+
+    result = find_longest_palindrome("defgaljdnki")
+    assert len(result) == 1
+    assert result in "defgaljdnki"
+
+
+def test_returns_whole_string_if_string_is_palindromic():
+    assert find_longest_palindrome("ababa") == "ababa"
+    assert find_longest_palindrome("defilopolifed") == "defilopolifed"
+    assert find_longest_palindrome("bb") == "bb"
+    assert find_longest_palindrome("greggerg") == "greggerg"
+
+
+def test_returns_longest_palindrome_from_middle_of_string():
+    result = find_longest_palindrome("abcbedlamirrorrimaldebalonmosq")
+    assert result == "bedlamirrorrimaldeb"
+
+    result = find_longest_palindrome("aaaagegaadecimallamicedgfgonto")
+    assert result == "decimallamiced"
+
+
+def test_returns_longest_palindrome_from_end_of_string():
+    result = find_longest_palindrome("ababacdegitalatiged")
+    assert result == "degitalatiged"
+
+    result = find_longest_palindrome("undertheseaamammal")
+    assert result == "mammal"
+
+
+def test_returns_longest_palindrome_from_beginning_of_string():
+    result = find_longest_palindrome("racecarracebike")
+    assert result == "racecar"
+
+    result = find_longest_palindrome("poordanisinadroopmappatella")
+    assert result == "poordanisinadroop"
