f1ce0a8efdc7bf8462665b41bc16ee691adb3dce
Donald Futers <donald.futers@gmail.com>
2025-02-24 15:43:00 +0000

    🔨 KATA 0013, pt. 4: slight refactor of longest_common_prefix()

--- src/kata0013/pt_4_longest_common_prefix.py
+++ src/kata0013/pt_4_longest_common_prefix.py
@@ -6,10 +6,8 @@ def longest_common_prefix(strs :list[str]) -> str:
     if not strs:
         return ""
     
-    i = 0
-    while i < len(strs[0]):
-        for j in range(1, len(strs)):
-            if i >= len(strs[j]) or strs[j-1][i] != strs[j][i]:
-                return strs[0][:i]  
-        i += 1
-    return strs[0][:i]
+    for i in range(len(strs[0])):
+        for st in strs:
+            if i == len(st) or st[i] != strs[0][i]:
+                return st[:i]
+    return strs[0]
