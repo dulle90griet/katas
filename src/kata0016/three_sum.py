17d26ba54481c8402ec17b1a6c4ff1bd6b8da3b2
Donald Futers <donald.futers@gmail.com>
2025-02-26 18:38:26 +0000

    ⚡️ KATA 0016: refactor three_sum() for a much more efficient three-pointer solution

--- src/kata0016/three_sum.py
+++ src/kata0016/three_sum.py
@@ -1,31 +1,33 @@
 def three_sum(nums: list[int]) -> list[list[int]]:
-    n = len(nums)
-    if n < 3:
-        return []
-    elif n == 3 and sum(nums) == 0:
-        return [nums]
-
-    checked = set()
-    sorted_nums = sorted(nums)
+    nums.sort()
     trios = []
 
-    def two_sum(target :int, ignore :int) -> list[int]:
-        num_map = {}
-        solutions = []
-        for j in range(ignore + 1, len(sorted_nums)):
-            if sorted_nums[j] not in checked:
-                complement = target - sorted_nums[j]
-                if complement in num_map:
-                    solution = [complement, sorted_nums[j]]
-                    if solution not in solutions:
-                        solutions.append(solution)
-                num_map[sorted_nums[j]] = j
-        return solutions
+    for i in range(len(nums) - 2):
+        if i > 0 and nums[i] == nums[i-1]:
+            continue
 
-    for i in range(n):
-        target = 0 - sorted_nums[i]
-        if target not in checked and sorted_nums[i] not in checked: 
-            trios += [[sorted_nums[i], x, y] for x, y in two_sum(target, i)]
-        checked.add(sorted_nums[i])
-    
-    return sorted(sorted(x) for x in trios)
+        if nums[i] > 0:
+            break
+
+        left, right = i + 1, len(nums) - 1
+
+        while left < right:
+            sum_trio = nums[i] + nums[left] + nums[right]
+            if sum_trio == 0:
+                trios.append([nums[i], nums[left], nums[right]])
+
+                while left < right and nums[left] == nums[left + 1]:
+                    left += 1
+                while left < right and nums[right] == nums[right - 1]:
+                    right -= 1
+
+                left += 1
+                right -= 1
+
+            elif sum_trio > 0:
+                right -= 1
+
+            else:
+                left += 1
+
+    return trios
