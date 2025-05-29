c3ee4d2bac0453981d835e5edcb2f0f95b86cc99
Donald Futers <donald.futers@gmail.com>
2025-05-29 13:18:48 +0100

    ✏️  remove obsolete comments from validate_brackets()

--- src/kata0020/validate_brackets.py
+++ src/kata0020/validate_brackets.py
@@ -1,11 +1,4 @@
 def validate_brackets(self, s: str) -> bool:
-    # create a structure of list nodes
-    # in each node, index 0 is a pointer to the parent node
-    # index 1 is the bracket type
-    # indexes 2+ are pointers to child nodes
-    # the top-level node has parent None
-
-    cur_node = [None, None]
     stack = []
     brackets = {
         ")": "(",
