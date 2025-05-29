40edcefc84f0c67ed2350a506fd8f74156b922c2
Donald Futers <donald.futers@gmail.com>
2025-05-29 13:49:50 +0100

    ✏️  add minor clarifying comment on logic to validate_brackets()

--- src/kata0020/validate_brackets.py
+++ src/kata0020/validate_brackets.py
@@ -11,6 +11,8 @@ def validate_brackets(self, s: str) -> bool:
             stack.append(c)
         else:
             if not stack or brackets[c] != stack.pop():
+                # If `s` can only contain brackets, `c` must be a closing bracket`
+                # And if the stack is empty, that bracket can't have been opened
                 return False
     
     return not stack
