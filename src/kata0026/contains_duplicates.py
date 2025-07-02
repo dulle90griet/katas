66963a9c2d26504d66c4ef9d21253534356fd450
Donald Futers <donald.futers@gmail.com>
2025-07-02 12:46:30 +0100

    🔨 KATA 0026: first implementation of contains_duplicate()

--- src/kata0026/contains_duplicates.py
+++ src/kata0026/contains_duplicates.py
@@ -1,14 +1,7 @@
-class Solution:
-    # first attempt
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        discovered = {}
-        for num in nums:
-            if num in discovered:
-                return True
-            discovered[num] = None
-        return False
-        ```
-
-	# more efficient
-	def containsDuplicate(self, nums: List\[int]) -> bool:
-        return len(set(nums)) != len(nums)
+def contains_duplicate(nums: list[int]) -> bool:
+    discovered = {}
+    for num in nums:
+        if num in discovered:
+            return True
+        discovered[num] = None
+    return False
