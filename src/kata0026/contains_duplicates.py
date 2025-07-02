71a4d478c51c3d8e2b4e8fa872c3bbdcdc9c8896
Donald Futers <donald.futers@gmail.com>
2025-07-02 15:14:36 +0100

    ⚡KATA 0026: complete more efficient implementation of contains_duplicates()

--- src/kata0026/contains_duplicates.py
+++ src/kata0026/contains_duplicates.py
@@ -1,7 +1,2 @@
 def contains_duplicate(nums: list[int]) -> bool:
-    discovered = {}
-    for num in nums:
-        if num in discovered:
-            return True
-        discovered[num] = None
-    return False
+    return len(set(nums)) != len(nums)
