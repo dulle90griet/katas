0ef885064a12aeef57d2e5ff5bcd365d41676329
Donald Futers <donald.futers@gmail.com>
2025-08-05 11:36:08 +0100

    🔨 KATA 0028: fully functioning but slow implementation of find_substrings_concatenating_all_words()

--- src/kata0028/find_substrings_concatenating_words.py
+++ src/kata0028/find_substrings_concatenating_words.py
@@ -7,23 +7,17 @@ def find_substrings_concatenating_all_words(
     substr_len = word_len * len(words)
     indexes = []
 
+    def get_frequencies(word_list: list[str]) -> dict:
+        frequencies = {}
+        for word in word_list:
+            frequencies[word] = frequencies.get(word, 0) + 1
+        return frequencies
+
+    frequencies = get_frequencies(words)
+
     for i in range(len(s) - substr_len + 1):
-        found = set()
-
-        for j in range(len(words)):
-            start = i + word_len * j
-            end = start + word_len
-
-            word = s[start:end]
-
-            if word not in words:
-                break
-            if word in found:
-                break
-
-            found.add(word)
-
-        if len(found) == len(words):
+        parts = [s[start:start+word_len] for start in range(i, i + substr_len, word_len)]
+        if get_frequencies(parts) == frequencies:
             indexes.append(i)
-    
+
     return indexes
