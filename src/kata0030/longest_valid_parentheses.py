22b5eb17d91c97e89de4a05b43ff3999db273eb8
Donald Futers <donald.futers@gmail.com>
2025-08-06 14:33:09 +0100

    ⚡KATA 0030: improve runtime of longest_valid_parentheses() by holding off reassingment of `result` until the new value is longer

--- src/kata0030/longest_valid_parentheses.py
+++ src/kata0030/longest_valid_parentheses.py
@@ -3,17 +3,20 @@ def longest_valid_parentheses(s: str) -> int:
     result = 0
 
     i = 0
-    for j in range(len(s)):
-        if s[j] == "(":
+    for j, ch in enumerate(s):
+        if ch == "(":
             stack.append(j)
         else:
             if not stack:
                 i = j + 1
                 continue
             if len(stack) == 1:
-                result = max(result, j - i + 1)
+                cur_len = max(result, j - i + 1)
             else:
-                result = max(result, j - stack[-2])
+                cur_len = max(result, j - stack[-2])
             stack.pop()
+
+            if cur_len > result:
+                result = cur_len
 
     return result
