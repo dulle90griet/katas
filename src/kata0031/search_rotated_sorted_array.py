916d0b0b847d4ce5bb7ce034ef88a46a3fddb5ac
Donald Futers <donald.futers@gmail.com>
2025-08-07 16:18:18 +0100

    ♻️  KATA 0031: refactor search_rotated_sorted_array() for O(log n) rather than O(n + 2(log n)) runtime

--- src/kata0031/search_rotated_sorted_array.py
+++ src/kata0031/search_rotated_sorted_array.py
@@ -1,31 +1,20 @@
 def search_rotated_sorted_array(nums: list[int], target: int) -> int:
-    pivot = 0
-
-    if nums[-1] < nums[0]:
-        left, right = 0, len(nums) - 1
-
-        while left < right:
-            middle = (left + right) // 2
-
-            if nums[middle] >= nums[0]:
-                left = middle + 1
-            else:
-                right = middle
-        
-        pivot = left
-
-        nums = nums[pivot:] + nums[:pivot]
-
     left, right = 0, len(nums) - 1
-    
+
     while left <= right:
         middle = (left + right) // 2
 
         if nums[middle] == target:
-            return (middle + pivot) % len(nums)
-        elif nums[middle] < target:
+            return middle
+
+        if (target >= nums[0]) == (nums[middle] >= nums[0]):
+            num = nums[middle]
+        else:
+            num = float('-inf') if nums[middle] >= nums[0] else float('inf') 
+
+        if num < target:
             left = middle + 1
         else:
             right = middle - 1
-
+    
     return -1
