0340ff68db1e8b88ff996ef61f2415cabc2754d0
Donald Futers <donald.futers@gmail.com>
2025-08-09 18:00:28 +0100

    🐛 KATA 0032: eliminate infinite loops and accommodate edge cases in right-bound finding logic, giving an implementation of find_first_and_last() that passes all tests

--- src/kata0032/find_first_and_last.py
+++ src/kata0032/find_first_and_last.py
@@ -19,12 +19,10 @@ def find_first_and_last(nums: list[int], target: int) -> tuple[int]:
 
     left_bound = right_bound = found
 
+    # find the leftmost matching number
     if found > 0:
-        # find the leftmost matching number
         l, r = 0, found - 1
-        if nums[r] < target:
-            left_bound = r + 1
-        else:
+        if nums[r] == target:
             while l < r:
                 m = (l + r) // 2
 
@@ -35,20 +33,29 @@ def find_first_and_last(nums: list[int], target: int) -> tuple[int]:
                 
             left_bound = l
 
-    if found < len(nums) - 1:
-        # find the rightmost matching number
+    # find the rightmost matching number
+    if found == len(nums) - 2:
+        if nums[found+1] == target:
+            right_bound = found+1
+    elif found < len(nums) - 2:
         l, r = found + 1, len(nums) - 1
-        if nums[l] > target:
-            right_bound = l - 1
-        else:
+
+        if nums[l] == target:
             while r > l:
+                if nums[l] == nums[r]:
+                    l = r
+                    break
+
+                if r - l == 1 and nums[l] != nums[r]:
+                    r = l
+
                 m = (l + r) // 2
 
-            if nums[m] > target:
-                r = m -1
-            else:
-                l = m
+                if nums[m] > target:
+                    r = m - 1
+                else:
+                    l = m
         
-            right_bound = r
+            right_bound = l
 
     return left_bound, right_bound
