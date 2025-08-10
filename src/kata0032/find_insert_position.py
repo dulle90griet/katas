2d5065fdab01aa6be4716eafd8aedb2c55c22ec3
Donald Futers <donald.futers@gmail.com>
2025-08-10 23:17:10 +0100

    🔨 KATA 0032: complete working implementation of find_insert_position()

---
+++ src/kata0032/find_insert_position.py
@@ -0,0 +1,14 @@
+def find_insert_position(nums: list[int], target: int) -> int:
+    left, right = 0, len(nums) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if nums[mid] == target:
+            return mid
+        if nums[mid] > target:
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return left
