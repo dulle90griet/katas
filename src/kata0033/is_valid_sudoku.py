9a99f4cdcfd63cebf48995d5f8eaa3283cb1234a
Donald Futers <donald.futers@gmail.com>
2025-08-11 12:11:07 +0100

    🔨 KATA 0033: inelegant first implementation of is_valid_sudoku()

---
+++ src/kata0033/is_valid_sudoku.py
@@ -0,0 +1,44 @@
+def is_valid_sudoku(board: list[list[str]]) -> bool:
+    nums = set([str(n) for n in range(1, 10)])
+
+    # iterate over rows (O(n))
+    for row in board:
+        count = {}
+        for tile in row:
+            if tile in nums:
+                count[tile] = count.get(tile, 0) + 1
+                if count[tile] > 1:
+                    return False
+        
+    # iterate over columns (O(n))
+    for col in range(len(board[0])):
+        count = {}
+        for row in board:
+            tile = row[col]
+            if tile in nums:
+                count[tile] = count.get(tile, 0) + 1
+                if count[tile] > 1:
+                    return False
+
+    # iterate over squares
+    def get_coords(n):
+        return n % 3, n // 3
+
+    for square in range(9):
+        count = {}
+        x, y = get_coords(square)
+        start_row = y * 3
+        start_col = x * 3
+
+        for subsquare in range(9):
+            x, y = get_coords(subsquare)
+            row = start_row + y
+            col = start_col + x
+
+            tile = board[row][col]
+            if tile in nums:
+                count[tile] = count.get(tile, 0) + 1
+                if count[tile] > 1:
+                    return False
+    
+    return True
