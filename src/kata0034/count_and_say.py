592468e9870eab7cf293e17bfa457c06d8609a0d
Donald Futers <donald.futers@gmail.com>
2025-08-12 15:56:22 +0100

    ⚡KATA 0034: memoize non-recursive count_and_say()

--- src/kata0034/count_and_say.py
+++ src/kata0034/count_and_say.py
@@ -1,7 +1,13 @@
+memo = {}
+
 def count_and_say(n: int) -> str:
     s = "1"
 
-    for _ in range(n - 1):
+    for m in range(n - 1):
+        if m in memo:
+            s = memo[m]
+            continue
+
         temp, count, prev = "", 0 , s[0]
         for char in s:
             if char == prev:
@@ -10,6 +16,7 @@ def count_and_say(n: int) -> str:
                 temp += f"{count}{prev}"
                 prev, count = char, 1
         temp += f"{count}{prev}"
-        s = temp
+        s = memo[m] = temp
 
     return s
+
