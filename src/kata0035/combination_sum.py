0046ff90143ac024fa89e7a5024690bd79c93519
Donald Futers <donald.futers@gmail.com>
2025-08-20 19:21:06 +0100

    🔨 KATA 0035: implement first working solution to combination_sum() using backtracking

--- src/kata0035/combination_sum.py
+++ src/kata0035/combination_sum.py
@@ -1,101 +1,17 @@
 def combination_sum(self, candidates: list[int], target: int) -> list[list[int]]:
-    # Generate all multiples
-    multiples = {}
-    result = []
+        ans, cur_combo = [], []
 
-    for i, num in enumerate(candidates):
-        if i == 0 or candidates[i-1] != num:
-            if num == 0:
-                if target == 0:
-                    result.append([0])
-                    break
-                multiples[0] = [[0]]
-                continue
-                
-            m = 1
-            while num * m <= target:
-                val, multiple = num * m, [num] * m
+        def combination(cur_sum, cur_idx):
+            if cur_sum > target:
+                return
+            if cur_sum == target:
+                ans.append([el for el in cur_combo])
 
-                if val == target:
-                    if multiple not in result:
-                        result.append(multiple)
-                    break
+            for i in range(cur_idx, len(candidates)):
+                cur_combo.append(candidates[i])
+                combination(cur_sum + candidates[i], i)
+                cur_combo.pop()
 
-                cur = multiples.get(val, [])
-                if multiple not in cur:
-                    cur.append(multiple)
-                multiples[val] = cur
-                
-                m += 1
+        combination(0, 0)
 
-
-    # Generate all possible combinations of multiples' values
-
-    nums = list(multiples.keys())
-    nums.sort()
-
-
-    def n_sum(target, n, start):
-        if len(nums) - start < n or target < nums[start] * n or target > nums[-1] * n:
-            return []
-        if n == 2:
-            return two_sum(target, start)
-        
-        res = []
-        for i in range(start, len(nums)-n+1):
-            if i == start or nums[i] != nums[i-1]:
-                for seq in n_sum(target-nums[i], n-1, i+1):
-                    res.append([nums[i]] + seq)
-        
-        return res
-
-
-    def two_sum(target, start):
-        duos = []
-        seen = set()
-
-        for i in range(start, len(nums)):
-            if len(duos) == 0 or duos[-1][1] != nums[i]:
-                if target - nums[i] in seen:
-                    duos.append([target - nums[i], nums[i]])
-                seen.add(nums[i])
-        
-        return duos
-
-    
-    combinations = []
-    for l in range(2, len(nums)+1):
-        l_combos = n_sum(target, l, 0)
-        if l_combos:
-            combinations += l_combos
-
-    
-    # Expand the combinations for all possible multiples
-    def combinate(combination, c=0):
-        if c >= len(combination):
-            return []
-        
-        combinated = []
-        for multiple in multiples[combination[c]]:
-            seqs = combinate(combination, c+1)
-            if seqs:
-                for seq in seqs:
-                    combinated.append(multiple + seq)
-            else:
-                combinated.append(multiple)
-
-        return combinated
-
-
-    expanded_combinations = []
-    for combination in combinations:
-        expanded_combinations += combinate(combination)
-
-
-    # Filter out repeated combinations
-    filtered_result = []
-    for solution in result + expanded_combinations:
-        if solution not in filtered_result:
-            filtered_result.append(solution)
-
-    return filtered_result
+        return ans
