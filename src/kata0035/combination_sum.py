57289d6c3c7df093e1489138d1cd8f79deb12212
Donald Futers <donald.futers@gmail.com>
2025-08-20 11:51:35 +0100

    🚨 KATA 0035: save much more elaborate but still failing attempt at combination_sum() - now handles combinations of multiple numbers but fails to filter out sequences with identical frequencies

--- src/kata0035/combination_sum.py
+++ src/kata0035/combination_sum.py
@@ -1,26 +1,101 @@
 def combination_sum(self, candidates: list[int], target: int) -> list[list[int]]:
-    sums = {}
+    # Generate all multiples
+    multiples = {}
     result = []
 
-    for c in candidates:
-        i = 1
-        while c * i <= target:
-            s = c * i
-            l = [c] * i
-            if s == target:
-                if l not in result:
-                    result.append(l)
-            else:
-                complement = target - s
-                if complement in sums:
-                    for combo in sums[complement]:
-                        if combo + l not in result:
-                            result.append(combo + l)
-                if s in sums:
-                    if l not in sums[s]:
-                        sums[s].append(l)
-                else:
-                    sums[s] = [l]
-            i += 1
+    for i, num in enumerate(candidates):
+        if i == 0 or candidates[i-1] != num:
+            if num == 0:
+                if target == 0:
+                    result.append([0])
+                    break
+                multiples[0] = [[0]]
+                continue
+                
+            m = 1
+            while num * m <= target:
+                val, multiple = num * m, [num] * m
 
-    return result
+                if val == target:
+                    if multiple not in result:
+                        result.append(multiple)
+                    break
+
+                cur = multiples.get(val, [])
+                if multiple not in cur:
+                    cur.append(multiple)
+                multiples[val] = cur
+                
+                m += 1
+
+
+    # Generate all possible combinations of multiples' values
+
+    nums = list(multiples.keys())
+    nums.sort()
+
+
+    def n_sum(target, n, start):
+        if len(nums) - start < n or target < nums[start] * n or target > nums[-1] * n:
+            return []
+        if n == 2:
+            return two_sum(target, start)
+        
+        res = []
+        for i in range(start, len(nums)-n+1):
+            if i == start or nums[i] != nums[i-1]:
+                for seq in n_sum(target-nums[i], n-1, i+1):
+                    res.append([nums[i]] + seq)
+        
+        return res
+
+
+    def two_sum(target, start):
+        duos = []
+        seen = set()
+
+        for i in range(start, len(nums)):
+            if len(duos) == 0 or duos[-1][1] != nums[i]:
+                if target - nums[i] in seen:
+                    duos.append([target - nums[i], nums[i]])
+                seen.add(nums[i])
+        
+        return duos
+
+    
+    combinations = []
+    for l in range(2, len(nums)+1):
+        l_combos = n_sum(target, l, 0)
+        if l_combos:
+            combinations += l_combos
+
+    
+    # Expand the combinations for all possible multiples
+    def combinate(combination, c=0):
+        if c >= len(combination):
+            return []
+        
+        combinated = []
+        for multiple in multiples[combination[c]]:
+            seqs = combinate(combination, c+1)
+            if seqs:
+                for seq in seqs:
+                    combinated.append(multiple + seq)
+            else:
+                combinated.append(multiple)
+
+        return combinated
+
+
+    expanded_combinations = []
+    for combination in combinations:
+        expanded_combinations += combinate(combination)
+
+
+    # Filter out repeated combinations
+    filtered_result = []
+    for solution in result + expanded_combinations:
+        if solution not in filtered_result:
+            filtered_result.append(solution)
+
+    return filtered_result
