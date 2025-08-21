43897974d19e1589e8d30f5c53b5f77c3080952d
Donald Futers <donald.futers@gmail.com>
2025-08-21 19:25:01 +0100

    🚚 KATA 0035: move DFS approach to combination_sum() to its own file

---
+++ src/kata0035/combination_sum_dfs.py
@@ -0,0 +1,17 @@
+def combination_sum(candidates: list[int], target: int) -> list[list[int]]:
+    ans, cur_combo = [], []
+
+    def combination(cur_sum, cur_idx):
+        if cur_sum > target:
+            return
+        if cur_sum == target:
+            ans.append(cur_combo[:])
+
+        for i in range(cur_idx, len(candidates)):
+            cur_combo.append(candidates[i])
+            combination(cur_sum + candidates[i], i)
+            cur_combo.pop()
+
+    combination(0, 0)
+
+    return ans
