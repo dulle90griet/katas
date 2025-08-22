766d3b6f654da068afe8d832959dea2b58f224c3
Donald Futers <donald.futers@gmail.com>
2025-08-22 23:36:59 +0100

    ♻️  KATA 0035: refactor DP combination_sum() to remove reundant if test

--- src/kata0035/combination_sum_dp.py
+++ src/kata0035/combination_sum_dp.py
@@ -1,10 +1,10 @@
 def combination_sum(candidates: list[int], target: int) -> list[list[int]]:
-    dp = [[] for n in range(target + 1)]
+    dp = [[] for _ in range(target + 1)]
     dp[0] = [[]]
 
     for c in candidates:
         for t in range(c, target+1):
-            if t - c == 0 or dp[t-c]:
+            if dp[t-c]:
                 for combo in dp[t-c]:
                     dp[t].append(combo + [c])
     
