ed155f7017eda6b0bfd0094b852c20ead434dbc7
Donald Futers <donald.futers@gmail.com>
2025-08-27 16:16:03 +0100

    🔨 KATA 0038: implement working solution to first_missing_positive()

---
+++ src/kata0038/first_missing_positive.py
@@ -0,0 +1,9 @@
+def first_missing_positive(nums: list[int]) -> int:
+    n = len(nums)
+    for i in range(n):
+        while nums[i] > 0 and nums[i] <= n and nums[i] != nums[nums[i]-1]:
+            nums[nums[i]-1], nums[i] = nums[i], nums[nums[i]-1]
+    for i in range(n):
+        if nums[i] != i + 1:
+            return i + 1
+    return n + 1
