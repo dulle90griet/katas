42cfee94b09c03d7a8211e4b26a78fa14730f1a6
Donald Futers <donald.futers@gmail.com>
2025-08-27 15:32:11 +0100

    ✨ KATA 0037: add foreach loop testing, command-line reps input, single-rep and multi-rep timing, rankings and prettier output to iteration_vs_enumeration.py

--- src/kata0037/iteration_vs_enumeration.py
+++ src/kata0037/iteration_vs_enumeration.py
@@ -2,6 +2,8 @@
 """
 
 import time
+import sys
+
 
 def time_func(func):
     def arg_handler(*args, **kwargs):
@@ -12,21 +14,50 @@ def time_func(func):
 
     return arg_handler
 
+
 @time_func
-def iterator(nums: list[int]) -> None:
+def range_iterator(nums: list[int]) -> None:
     for i in range(len(nums)):
-        idx = i
-        num = nums[i]
+        res = f"{i}: {nums[i]}"
+
+
+@time_func
+def foreach_iterator(nums: list[int]) -> None:
+    i = 0
+    for num in nums:
+        res = f"{i}: {num}"
+        i += 1
+
 
 @time_func
 def enumerator(nums: list[int]) -> None:
     for i, x in enumerate(nums):
-        idx = i
-        num = x
+        res = f"{i}: {x}"
 
 
-nums = list(range(10**6))
+scale = 10**6
+nums = list(range(scale))
+reps = int(sys.argv[1])
 
-print(iterator(nums))
-print(enumerator(nums))
+print(f"Iterating over {scale} numbers...\n")
+
+print("SINGLE REP\n==========")
+rep_times = {
+    "range": range_iterator(nums),
+    "foreach": foreach_iterator(nums),
+    "enum": enumerator(nums)
+}
+sorted_times = sorted(list(rep_times.keys()), key=lambda x: rep_times[x])
+for i, key in enumerate(sorted_times):
+    print(f" {i+1}. {key} ({rep_times[key]:0.6f}s)")
+
+print(f"\n{reps}-REP AVG\n"+"="*(8+len(str(reps))))
+rep_times = {
+    "range": sum([range_iterator(nums) for _ in range(reps)]) / reps,
+    "foreach": sum([foreach_iterator(nums) for _ in range(reps)]) / reps,
+    "enum": sum([enumerator(nums) for _ in range(reps)]) / reps
+}
+sorted_times = sorted(list(rep_times.keys()), key=lambda x: rep_times[x])
+for i, key in enumerate(sorted_times):
+    print(f" {i+1}. {key} ({rep_times[key]:0.6f}s)")
 
