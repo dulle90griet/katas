8a0737bb55211804038868e4fee5a9e29174006d
Donald Futers <donald.futers@gmail.com>
2025-08-28 14:21:25 +0100

    ⚡KATA 0039: refactor trap_water() for improved performance and O(n) runtime complexity

--- src/kata0039/trap_water.py
+++ src/kata0039/trap_water.py
@@ -1,35 +1,34 @@
 def trap_water(height: list[int]) -> int:
-        trapped = 0
-        hmap = {}
+    trapped, basin, left = 0, 0, 0
 
-        for i, h in enumerate(height):
-            if h > 0:
-                if h not in hmap:
-                    hmap[h] = [i, i, 1]
-                else:
-                    hmap[h][1] = i
-                    hmap[h][2] += 1
-        
-        hlist = sorted(list(hmap.keys()), reverse=True)
-        if not hlist:
-            return 0
+    for right in range(1, len(height)):
+        if height[right] == 0:
+            continue
 
-        hlist.append(0)
-        for i in range(len(hlist) - 1):
-            hmap[hlist[i]].append(hlist[i] - hlist[i+1])
-        
-        for i, h in enumerate(hlist):
-            if hmap[h][2] > 1:
-                trapped += (hmap[h][1] - hmap[h][0] - hmap[h][2] + 1) * hmap[h][3]
+        if height[right] >= height[left]:
+            width = right - (left + 1)
+            trapped += width * min(height[left], height[right]) - basin
+            left = right
+            basin = 0
+        else:
+            basin += height[right]
 
-            if i == len(hlist) - 2:
-                break
-
-            hmap[hlist[i+1]][2] += hmap[h][2]
-
-            if hmap[h][0] < hmap[hlist[i+1]][0]:
-                hmap[hlist[i+1]][0] = hmap[h][0]
-            if hmap[h][1] > hmap[hlist[i+1]][1]:
-                hmap[hlist[i+1]][1] = hmap[h][1]
-        
+    if left == len(height) - 1:
         return trapped
+
+    last_left = left
+    basin, right = 0, len(height)-1
+
+    for left in range(len(height)-2, last_left-1, -1):
+        if height[left] == 0:
+            continue
+
+        if height[left] >= height[right]:
+            width = right - (left + 1)
+            trapped += width * min(height[left], height[right]) - basin
+            right = left
+            basin = 0
+        elif right != left:
+            basin += height[left]
+
+    return trapped
