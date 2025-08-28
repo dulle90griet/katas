b6a5591430a50dcd58bc04f6ddcae1608a0a8d3d
Donald Futers <donald.futers@gmail.com>
2025-08-28 16:32:30 +0100

    ⚡KATA 0039: complete refactored implementation of trap_water(): simplified logic, O(n) time complexity, O(1) space complexity, single-digit runtime

--- src/kata0039/trap_water.py
+++ src/kata0039/trap_water.py
@@ -1,23 +1,19 @@
 def trap_water(height: list[int]) -> int:
-    trapped = 0
+    trapped = max_left = max_right = 0
     left, right = 0, len(height) - 1
-    prev_lowest = 0
 
     while left < right:
-        cur_lowest = min(height[left], height[right])
-        if cur_lowest > prev_lowest:
-            height_diff = cur_lowest - prev_lowest
-            width = right - (left + 1)
-            trapped += width * height_diff
-            prev_lowest = cur_lowest
-
-        if height[right] >= height[left]:
+        if height[left] <= height[right]:
+            if height[left] > max_left:
+                max_left = height[left]
+            else:
+                trapped += max_left - height[left]
             left += 1
-            if trapped > 0 and right > left:
-                trapped -= min(height[left], prev_lowest)
         else:
+            if height[right] > max_right:
+                max_right = height[right]
+            else:
+                trapped += max_right - height[right]
             right -= 1
-            if trapped > 0 and right > left:
-                trapped -= min(height[right], prev_lowest)
     
     return trapped
