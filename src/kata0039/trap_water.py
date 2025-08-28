8ae7307e1b08b4c65b675269f771d9d5efe6e633
Donald Futers <donald.futers@gmail.com>
2025-08-28 13:13:57 +0100

    🔨 KATA 0039: implement passing solution to trap_water() - works, but prodigiously slowly; complexity is O(n log n) since it uses sorted()

--- src/kata0039/trap_water.py
+++ src/kata0039/trap_water.py
@@ -1,29 +1,35 @@
 def trap_water(height: list[int]) -> int:
-    trapped = 0
-
-    n = 1
-    while True:
-        row_tiles = 0
-        is_wall = False
-        is_basin = False
-        last_wall = 0
+        trapped = 0
+        hmap = {}
 
         for i, h in enumerate(height):
-            if h >= n:
-                if is_basin:
-                    trapped += i - last_wall - 1
-                    is_basin = False
-                is_wall = True
-                last_wall = i
-                row_tiles += 1
-            else:
-                if is_wall:
-                    is_basin = True
-                is_wall = False
+            if h > 0:
+                if h not in hmap:
+                    hmap[h] = [i, i, 1]
+                else:
+                    hmap[h][1] = i
+                    hmap[h][2] += 1
         
-        if row_tiles <= 1:
-            break
+        hlist = sorted(list(hmap.keys()), reverse=True)
+        if not hlist:
+            return 0
+
+        hlist.append(0)
+        for i in range(len(hlist) - 1):
+            hmap[hlist[i]].append(hlist[i] - hlist[i+1])
         
-        n += 1
-    
-    return trapped
+        for i, h in enumerate(hlist):
+            if hmap[h][2] > 1:
+                trapped += (hmap[h][1] - hmap[h][0] - hmap[h][2] + 1) * hmap[h][3]
+
+            if i == len(hlist) - 2:
+                break
+
+            hmap[hlist[i+1]][2] += hmap[h][2]
+
+            if hmap[h][0] < hmap[hlist[i+1]][0]:
+                hmap[hlist[i+1]][0] = hmap[h][0]
+            if hmap[h][1] > hmap[hlist[i+1]][1]:
+                hmap[hlist[i+1]][1] = hmap[h][1]
+        
+        return trapped
