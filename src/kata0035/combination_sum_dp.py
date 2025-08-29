25750176ff37a77ff0223bfcf4811c891aa762b9
Donald Futers <donald.futers@gmail.com>
2025-08-29 11:28:41 +0100

    ♻️  KATA 0035: refactor DP combination_sum() to eliminate redundant `if` and streamline code with a comprehension

--- src/kata0035/combination_sum_dp.py
+++ src/kata0035/combination_sum_dp.py
@@ -4,8 +4,6 @@ def combination_sum(candidates: list[int], target: int) -> list[list[int]]:
 
     for c in candidates:
         for t in range(c, target+1):
-            if dp[t-c]:
-                for combo in dp[t-c]:
-                    dp[t].append(combo + [c])
+            dp[t] += [combo + [c] for combo in dp[t-c]]
     
     return dp[target]
