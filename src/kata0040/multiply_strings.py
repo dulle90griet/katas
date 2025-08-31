c84254030e82164b32d4a08d4233781abd8a8332
Donald Futers <donald.futers@gmail.com>
2025-08-31 14:05:26 +0100

    ⚡KATA 0040: refactor multiply_strings() and improve runtime to O(len(n) * len(m))

--- src/kata0040/multiply_strings.py
+++ src/kata0040/multiply_strings.py
@@ -2,50 +2,20 @@ def multiply_strings(num1: str, num2: str) -> str:
     if num1 == "0" or num2 == "0":
         return "0"
 
-    int_map = {
-        "0": 0,
-        "1": 1,
-        "2": 2,
-        "3": 3,
-        "4": 4,
-        "5": 5,
-        "6": 6,
-        "7": 7,
-        "8": 8,
-        "9": 9
-    }
+    m = {"0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6,
+         "7": 7, "8": 8, "9": 9, 0: "0", 1: "1", 2: "2", 3: "3",
+         4: "4", 5: "5", 6: "6", 7: "7", 8: "8", 9: "9"}
 
-    res = []
-    for j in range(len(num2)):
-        carry = 0
-        cur = "0" * j
-
-        for i in range(len(num1)-1, -1, -1):
-            d_1 = int_map[num1[i]]
-            d_2 = int_map[num2[len(num2) - 1 - j]]
-
-            val = d_1 * d_2 + carry
-            cur = f"{val % 10}{cur}"
-            carry = val // 10
-
-        if carry > 0:
-            cur = f"{carry}{cur}"
-
-        res.append(cur)
-
-    max_len = max(len(r) for r in res)
+    res = [0] * (len(num1) + len(num2))
+    for j, c2 in enumerate(reversed(num2)):
+        for i, c1 in enumerate(reversed(num1)):
+            d1 = m[c1]
+            d2 = m[c2]
+            res[i + j] += d1 * d2
+            res[i + j + 1] += res[i + j] // 10
+            res[i + j] %= 10
     
-    ans, carry, i = "", 0, 0
-    for i in range(max_len):
-        cur = carry
-        for r in res:
-            if i < len(r):
-                cur += int_map[r[-(i+1)]]
+    while len(res) > 1 and res[-1] == 0:
+        res.pop()
 
-        ans = f"{cur % 10}{ans}"
-        carry = cur // 10     
-
-    if carry > 0:
-        ans = f"{carry}{ans}"
-
-    return ans
+    return "".join(m[c] for c in res)[::-1]
