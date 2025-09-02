b1a505082a1312bed897852abf2c09d1ced2dbdd
Donald Futers <donald.futers@gmail.com>
2025-09-02 19:46:56 +0100

    🔨 KATA 0041: implement working solution to wildcard_match() with O(mn) runtime complexity

--- src/kata0041/wildcard_match.py
+++ src/kata0041/wildcard_match.py
@@ -1,34 +1,23 @@
 def wildcard_match(s: str, p: str) -> bool:
-    matches = []
+    s_idx = p_idx = 0
+    last_match = star_p = -1
 
-    def dfs(s_idx, p_idx):
-        if bool(matches):
-            return
+    while s_idx < len(s):
+        if p_idx < len(p) and (s[s_idx] == p[p_idx] or p[p_idx] == "?"):
+            s_idx += 1
+            p_idx += 1
+        elif p_idx < len(p) and p[p_idx] == "*":
+            star_p = p_idx
+            p_idx += 1
+            last_match = s_idx
+        elif star_p != -1:
+            p_idx = star_p + 1
+            last_match += 1
+            s_idx = last_match
+        else:
+            return False
 
-        if p_idx >= len(p) and s_idx >= len(s):
-            matches.append(1)
-            return
-        if s_idx >= len(s):
-            if all([c == "*" for c in p[p_idx:]]):
-                matches.append(1)
-            return
-        if p_idx >= len(p):
-            return
-
-        match p[p_idx]:
-            case "*":
-                dfs(s_idx+1, p_idx+1) # Wildcard matched single char; continue to next string and pattern chars
-                dfs(s_idx+1, p_idx) # Wildcard match group continues to next string char
-                if p_idx < len(p) - 1:
-                    dfs(s_idx, p_idx+1) # Wildcard matched empty sequence; continue to next pattern char
-            case "?":
-                dfs(s_idx+1, p_idx+1)
-            case _:
-                if s[s_idx] == p[p_idx]:
-                    dfs(s_idx+1, p_idx+1)
-                else:
-                    return
+    while p_idx < len(p) and p[p_idx] == "*":
+        p_idx += 1
     
-    dfs(0, 0)
-
-    return bool(matches)
+    return p_idx == len(p)
