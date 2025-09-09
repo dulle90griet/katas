27ec0efd508ab8857c955c798b22056ee0626623
Donald Futers <donald.futers@gmail.com>
2025-09-10 00:12:38 +0100

    🚨 KATA 0042: implement jump_2() solution, passing small-scale tests but timing out for complex inputs

--- src/kata0042/jump_ii.py
+++ src/kata0042/jump_ii.py
@@ -1,12 +1,12 @@
-def jump_2(self, nums: List[int]) -> int:
-    ans = []
-
+def jump_2(nums: list[int]) -> int:
     def dfs(nums, start, limit, ans, path):
         if start == len(nums) - 1:
-            if ans and path and len(path) < len(ans):
+            if ans == []:
+                ans.append(path[:])
+            elif len(path) < len(ans[-1]):
                 ans.pop()
                 ans.append(path[:])
-                return
+            return
         elif start >= len(nums):
             return
         
@@ -15,7 +15,8 @@ def jump_2(self, nums: List[int]) -> int:
             path.append(i)
             dfs(nums, i, nums[i], ans, path)
             path.pop()
-    
+
+    ans = []   
     dfs(nums, 0, nums[0], ans, [])
 
-    return dfs
+    return len(ans[-1])
