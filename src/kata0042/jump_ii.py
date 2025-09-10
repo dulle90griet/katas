65701da1d6d0ca32a5c883504220b19419992a3b
Donald Futers <donald.futers@gmail.com>
2025-09-10 16:45:02 +0100

    🔨 KATA 0042: implement passing solution to jump_2()

--- src/kata0042/jump_ii.py
+++ src/kata0042/jump_ii.py
@@ -1,22 +1,10 @@
 def jump_2(nums: list[int]) -> int:
-    def dfs(nums, start, limit, ans, path):
-        if start == len(nums) - 1:
-            if ans == []:
-                ans.append(path[:])
-            elif len(path) < len(ans[-1]):
-                ans.pop()
-                ans.append(path[:])
-            return
-        elif start >= len(nums):
-            return
-        
-        limit = min(len(nums) - 1 - start, limit)
-        for i in range(start+1, start+limit+1):
-            path.append(i)
-            dfs(nums, i, nums[i], ans, path)
-            path.pop()
-
-    ans = []   
-    dfs(nums, 0, nums[0], ans, [])
-
-    return len(ans[-1])
+    if len(nums) <= 1:
+        return 0
+    l, r = 0, nums[0]
+    jumps = 1
+    while r < len(nums) - 1:
+        jumps += 1
+        nxt = max(i + nums[i] for i in range(l, r + 1))
+        l, r = r, nxt
+    return times
