dbb07f00a8a825517c2fe581c3c7913161af1731
Donald Futers <donald.futers@gmail.com>
2025-09-13 18:24:58 +0100

    ⚡KATA 0043: refactor permute() solution for improved efficiency, using swapping instead of a set to keep track of already-used digits

--- src/kata0043/permute.py
+++ src/kata0043/permute.py
@@ -1,22 +1,14 @@
 def permute(nums: list[int]) -> list[list[int]]:
-    def backtrack(
-        nums: list[int], 
-        used: set[int], 
-        ans: list[list[int]], 
-        path: list[int]
-    ) -> None:
-        if len(path) == len(nums):
-            ans.append(path[:])
+    def backtrack(nums: list[int], start: int, ans: list[list[int]]) -> None:
+        if start >= len(nums):
+            ans.append(nums[:])
             return
         
-        for i, n in enumerate(nums):
-            if i not in used:
-                used |= {i}
-                path.append(n)
-                backtrack(nums, used, ans, path)
-                path.pop()
-                used -= {i}
+        for i in range(start, len(nums)):
+            nums[i], nums[start] = nums[start], nums[i]
+            backtrack(nums, start+1, ans)
+            nums[i], nums[start] = nums[start], nums[i]
     
     ans = []
-    backtrack(nums, set(), ans, [])
+    backtrack(nums, 0, ans)
     return ans
