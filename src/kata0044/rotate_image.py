921a5605bc4f5910163d5b21288710b1a4fc302f
Donald Futers <donald.futers@gmail.com>
2025-09-16 00:53:07 +0100

    🔨 KATA 0044: implement initial solution to rotate_image(), passing tests on odd-sided matrices - even-sided matrices next

---
+++ src/kata0044/rotate_image.py
@@ -0,0 +1,26 @@
+def rotate_image(matrix: list[list[int]]) -> None:
+    m = {(0, 1): "r", (1, 0): "d", (0, -1): "l", (-1, 0): "u"}
+
+    c = len(matrix[0]) // 2
+
+    for i in range(1, len(matrix[0]) - c):
+        mov = (0, 1) # Start moving right
+        x = y = c - i
+
+        for shift in range(2 * i):
+            last = matrix[c-i][c-i]
+
+            while True:
+                if (m[mov] == "r" and x == c + i) or (m[mov] == "d" and y == c + i) or (m[mov] == "l" and x == c - i):
+                    # 90 degree CW transformation of 2D vector when y-axis increases downwards
+                    # is (y, x) -> (x, -y)
+                    mov = (mov[1], -mov[0])
+                elif m[mov] == "u" and y == c - 1:
+                    # one shift circuit completed
+                    mov = (mov[1], -mov[0])
+                    break
+                
+                x += mov[1]
+                y += mov[0]
+
+                last, matrix[y][x] = matrix[y][x], last
