24367660a186ac2515d91bd2cd4796b759512ab6
Donald Futers <donald.futers@gmail.com>
2025-09-16 17:08:53 +0100

    ⚡KATA 0044: refactor rotate_image() for greater efficiency by swapping pixels directly into rotated position - beats 100% on LeetCode, with O(n) runtime complexity and O(1) spatial complexity

--- src/kata0044/rotate_image.py
+++ src/kata0044/rotate_image.py
@@ -1,27 +1,30 @@
 def rotate_image(matrix: list[list[int]]) -> None:
-    m = {(0, 1): "r", (1, 0): "d", (0, -1): "l", (-1, 0): "u"}
-
     c = len(matrix[0]) // 2
-    rdiff = 0 if len(matrix[0]) % 2 else -1
+    c_width = 1 if len(matrix[0]) % 2 else 0
 
-    for i in range(1, c + 1):
-        mov = (0, 1) # Start moving right
-        x = y = c - i
+    for layer in range(1, c + 1):
+        layer_size = 2 * layer + c_width
 
-        for shift in range(2 * i + rdiff):
-            last = matrix[c-i][c-i]
+        for pixel in range(layer_size - 1):
+            x_offset, y_offset = pixel - layer, -layer
 
-            while True:
-                if (m[mov] == "r" and x == c + i + rdiff) or (m[mov] == "d" and y == c + i + rdiff) or (m[mov] == "l" and x == c - i):
-                    # 90 degree CW transformation of 2D vector when y-axis increases downwards
-                    # is (y, x) -> (x, -y)
-                    mov = (mov[1], -mov[0])
-                elif m[mov] == "u" and y == c - i:
-                    # one shift circuit completed
-                    mov = (mov[1], -mov[0])
-                    break
-                
-                x += mov[1]
-                y += mov[0]
+            last = matrix[c + y_offset][c + x_offset]
 
-                last, matrix[y][x] = matrix[y][x], last
+            for side in range(4):
+                # 90 degree CW transformation of 2D vector when y-axis
+                # increases downwards is (x, y) -> (-y, x)
+                x_offset, y_offset = -y_offset, x_offset
+
+                swap_x, swap_y = c + x_offset, c + y_offset
+
+                if not c_width:
+                    match side:
+                        case 0:
+                            swap_x -= 1
+                        case 1:
+                            swap_x -= 1
+                            swap_y -= 1
+                        case 2:
+                            swap_y -= 1
+
+                last, matrix[swap_y][swap_x] = matrix[swap_y][swap_x], last
