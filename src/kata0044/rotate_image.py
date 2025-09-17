6e8a1d578abcbdc2ff1bd3def12bdd66b1a64676
Donald Futers <donald.futers@gmail.com>
2025-09-17 10:32:18 +0100

    ♻ KATA 0044: refactor rotate_image() for ultra-condensed solution

--- src/kata0044/rotate_image.py
+++ src/kata0044/rotate_image.py
@@ -1,30 +1,6 @@
 def rotate_image(matrix: list[list[int]]) -> None:
-    c = len(matrix[0]) // 2
-    c_width = 1 if len(matrix[0]) % 2 else 0
-
-    for layer in range(1, c + 1):
-        layer_size = 2 * layer + c_width
-
-        for pixel in range(layer_size - 1):
-            x_offset, y_offset = pixel - layer, -layer
-
-            last = matrix[c + y_offset][c + x_offset]
-
-            for side in range(4):
-                # 90 degree CW transformation of 2D vector when y-axis
-                # increases downwards is (x, y) -> (-y, x)
-                x_offset, y_offset = -y_offset, x_offset
-
-                swap_x, swap_y = c + x_offset, c + y_offset
-
-                if not c_width:
-                    match side:
-                        case 0:
-                            swap_x -= 1
-                        case 1:
-                            swap_x -= 1
-                            swap_y -= 1
-                        case 2:
-                            swap_y -= 1
-
-                last, matrix[swap_y][swap_x] = matrix[swap_y][swap_x], last
+    n = len(matrix)
+    for i in range(n//2):
+        for j in range(n-n//2):
+            matrix[i][j], matrix[~j][i], matrix[~i][~j], matrix[j][~i] = \
+                        matrix[~j][i], matrix[~i][~j], matrix[j][~i], matrix[i][j]
