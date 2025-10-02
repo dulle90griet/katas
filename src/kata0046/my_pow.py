881de84e22f8cabbf3e31135d070307d2d15cfef
Donald Futers <donald.futers@gmail.com>
2025-10-02 13:53:25 +0100

    🔨 KATA 0046: implement passing recursive approach to my_pow()

---
+++ src/kata0046/my_pow.py
@@ -0,0 +1,8 @@
+def my_pow(x: float, n: int) -> float:
+    if n < 0:
+        n = abs(n)
+        x = 1 / x
+    res = 1
+    for _ in range(n):
+        res *= x
+    return res
--- src/kata0046/pow.py
+++ src/kata0046/pow.py
@@ -1,8 +1,13 @@
-def pow(x: float, n: int) -> float:
+def my_pow(x: float, n: int) -> float:
+    if n == 0:
+        return 1
     if n < 0:
-        n = abs(n)
         x = 1 / x
-    res = 1
-    for _ in range(n):
-        res *= x
-    return res
+        n = -n
+    
+    res = my_pow(x, n//2)
+
+    if n % 2:
+        return res * res * x
+    else:
+        return res * res
