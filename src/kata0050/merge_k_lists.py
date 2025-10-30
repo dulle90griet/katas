b02104a670ef040f9d040b0e55c1bdbce8040591
Donald Futers <donald.futers@gmail.com>
2025-10-30 11:50:26 +0000

    ⚡KATA 0050: refactor merge_k_lists() for dramatically improved performance - O(n log k) complexity for n items in k lists

--- src/kata0050/merge_k_lists.py
+++ src/kata0050/merge_k_lists.py
@@ -8,19 +8,40 @@ class ListNode:
 
 
 def merge_k_lists(lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-    dummy = head = ListNode(None)
+    def merge_down(lists_to_merge):
+        if len(lists_to_merge) == 0:
+            return None
+        if len(lists_to_merge) == 1:
+            return lists_to_merge[0]
 
-    while True:
-        for i in range(len(lists)-1, -1, -1):
-            if not lists[i]:
-                lists.pop(i)
-        if not lists:
-            break
-            
-        lowest = min(lists, key=lambda x: x.val)
-        min_idx = lists.index(lowest)
-        head.next = ListNode(lists[min_idx].val)
-        head = head.next
-        lists[min_idx] = lists[min_idx].next
-    
-    return dummy.next
+        merged_lists = []
+
+        for i in range(0, len(lists_to_merge), 2):
+            merged_dummy = merged_head = ListNode()
+
+            list_1_head = lists_to_merge[i]
+            list_2_head = lists_to_merge[i+1] if i+1 < len(lists_to_merge) else None
+
+            while list_1_head or list_2_head:
+                if list_2_head is None:
+                    merged_head.next = ListNode(list_1_head.val)
+                    merged_head = merged_head.next
+                    list_1_head = list_1_head.next
+                elif list_1_head is None:
+                    merged_head.next = ListNode(list_2_head.val)
+                    merged_head = merged_head.next
+                    list_2_head = list_2_head.next
+                elif list_1_head.val <= list_2_head.val:
+                    merged_head.next = ListNode(list_1_head.val)
+                    merged_head = merged_head.next
+                    list_1_head = list_1_head.next
+                else:
+                    merged_head.next = ListNode(list_2_head.val)
+                    merged_head = merged_head.next
+                    list_2_head = list_2_head.next
+
+            merged_lists.append(merged_dummy.next)
+        
+        return merge_down(merged_lists)
+
+    return merge_down(lists)
