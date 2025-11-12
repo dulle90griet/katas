1fb53a0262e528e03f659dc4e5837f803c6aa8e0
Donald Futers <donald.futers@gmail.com>
2025-11-12 18:55:53 +0000

    🔨 KATA 0053: implement passing solution to min_operations() - time complexity O(n)

--- src/kata0053/min_operations.py
+++ src/kata0053/min_operations.py
@@ -1,29 +1,16 @@
 def min_operations(nums: list[int]) -> int:
-    def count_ops(start, end):
-        op_count = 0
-        base_positions = []
-        span_min = float('inf')
+    stack = []
+    ans = 0
 
-        for i in range(start, end):
-            if nums[i] < span_min:
-                span_min = nums[i]
-                base_positions = [start-1, i]
-            elif nums[i] == span_min:
-                base_positions.append(i)
-        base_positions.append(end)
-
-        if span_min > 0:
-            op_count += 1
-            
-        for p in range(1, len(base_positions)):
-            span_start = base_positions[p-1] + 1
-            span_end = base_positions[p]
-
-            if span_end - span_start == 1:
-                op_count += 1
-            elif span_end - span_start > 1:
-                op_count += count_ops(span_start, span_end)
+    for num in nums:
+        while len(stack) > 0 and num < stack[-1]:
+            stack.pop()
         
-        return op_count
+        if len(stack) == 0 or num > stack[-1]:
+            if num == 0:
+                continue
+
+            stack.append(num)
+            ans += 1
     
-    return count_ops(0, len(nums))
+    return ans
